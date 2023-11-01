5b5582a51488d75156c91b7d13f86b45864b7afa
pom-suke <tynnw810@yahoo.co.jp>
2023-11-02 08:51:53 +0900

    新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :orders
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :nickname, presence: true
+  validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }, presence: true
+  validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }, presence: true
+  validates :kana_last_name, format: { with: /\A[ァ-ヶー－]+\z/ }, presence: true
+  validates :kana_first_name, format: { with: /\A[ァ-ヶー－]+\z/ }, presence: true
+  validates :birthday, presence: true
 end

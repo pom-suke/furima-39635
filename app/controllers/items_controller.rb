8fb619e5179d3d60d50dc0f3e8aff19a1e54f474
pom-suke <tynnw810@yahoo.co.jp>
2023-11-08 19:01:29 +0900

    商品出品ページの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,27 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :product, :describe, :category_id, :status_id, :pay_shipping_id, :prefecture_id, :until_shipping_id, :price, :image
+    ).merge(user_id: current_user.id)
+  end
+
 end

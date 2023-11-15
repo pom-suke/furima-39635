cac936c20fd59eaddfa0faa0a8c68fee2e848a17
pom-suke <tynnw810@yahoo.co.jp>
2023-11-15 14:50:17 +0900

    販売手数料と販売利益の表示の実装

---
+++ app/javascript/fee.js
@@ -0,0 +1,15 @@
+function fee (){
+  const itemPrice = document.getElementById("item-price")
+  if (itemPrice) {
+    itemPrice.addEventListener("input", ()=>{
+      const inputValue = itemPrice.value
+      const addTaxPrice = document.getElementById("add-tax-price")
+      const calcAddTax = Math.floor(inputValue * 0.1)
+      addTaxPrice.innerHTML = calcAddTax.toLocaleString()
+      const profit = document.getElementById("profit")
+      profit.innerHTML = (inputValue - calcAddTax).toLocaleString()
+    })
+  }
+}
+
+window.addEventListener('turbo:load', fee)

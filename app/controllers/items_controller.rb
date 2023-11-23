339402df1148a77bb4b92ba39dafb599b7c95817
pom-suke <tynnw810@yahoo.co.jp>
2023-11-24 03:37:43 +0900

    編集ページに直接アクセスさせない処理の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only:[:new, :edit]
+  before_action :move_to_index, only: :edit
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -42,6 +43,13 @@ class ItemsController < ApplicationController
     params.require(:item).permit(
       :product, :describe, :category_id, :status_id, :pay_shipping_id, :prefecture_id, :until_shipping_id, :price, :image
     ).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to action: :index
+    end
   end
 
 end

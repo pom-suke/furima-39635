22e70579f3a892cf4e4616dc20ce5f200534c55f
pom-suke <tynnw810@yahoo.co.jp>
2023-12-05 23:58:30 +0900

    不要なカラムの削除

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -17,6 +17,6 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :order_date).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,13 +1,12 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :order_id, :order_date, :user_id, :item_id
+  attr_accessor :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :order_id, :user_id, :item_id
 
   with_options presence: true do
     validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, message: "はハイフンを含めた半角数字で入力してください" }
     validates :city
     validates :house_number
     validates :order_id
-    validates :order_date
     validates :user_id
     validates :item_id
   end
@@ -15,7 +14,7 @@ class OrderAddress
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
 
   def save
-    order = Order.create(order_date: order_date, user_id: user_id, item_id: item_id)
+    order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end

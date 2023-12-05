d45711129640f38dcae85f8ab1c87ac2f53eaae7
pom-suke <tynnw810@yahoo.co.jp>
2023-12-05 23:36:07 +0900

    コントローラーとアソシエーションの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,13 +1,15 @@
 class OrdersController < ApplicationController
   def index
     @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       @order_address.save
-      redirect_to root_path
+      redirect_to items_path
     else
       render :index, status: :unprocessable_entity
     end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   has_one :order
+  has_one :address, through: :order
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
